d59aad666877a3e4beba9e25d594cf4937ee5c61
pkm <parag@pkmital.com>
2016-07-25 13:54:31 -0700

    updating crop function bug

--- session-1/libs/utils.py
+++ session-1/libs/utils.py
@@ -28,11 +28,17 @@ def imcrop_tosquare(img):
         Cropped image.
     """
     if img.shape[0] > img.shape[1]:
-        extra = (img.shape[0] - img.shape[1]) // 2
-        crop = img[extra:-extra, :]
+        extra = (img.shape[0] - img.shape[1])
+        if extra % 2 == 0:
+            crop = img[extra // 2:-extra // 2, :]
+        else:
+            crop = img[max(0, extra // 2 - 1):min(-1, -extra // 2), :]
     elif img.shape[1] > img.shape[0]:
-        extra = (img.shape[1] - img.shape[0]) // 2
-        crop = img[:, extra:-extra]
+        extra = (img.shape[1] - img.shape[0])
+        if extra % 2 == 0:
+            crop = img[:, extra // 2:-extra // 2]
+        else:
+            crop = img[:, max(0, extra // 2 - 1):min(-1, -extra // 2)]
     else:
         crop = img
     return crop
