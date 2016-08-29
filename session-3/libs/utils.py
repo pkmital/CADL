91ee051f294d7e9397ca07cd838b733bfbf78041
pkm <parag@pkmital.com>
2016-08-29 10:05:08 -0700

    updating vae to use epochs; issue w/ test_mnist not using valid split

--- session-3/libs/utils.py
+++ session-3/libs/utils.py
@@ -15,6 +15,74 @@ import os
 from scipy.io import wavfile
 
 
+def download(path):
+    """Use urllib to download a file.
+
+    Parameters
+    ----------
+    path : str
+        Url to download
+
+    Returns
+    -------
+    path : str
+        Location of downloaded file.
+    """
+    import os
+    from six.moves import urllib
+
+    fname = path.split('/')[-1]
+    if os.path.exists(fname):
+        return fname
+
+    print('Downloading ' + path)
+
+    def progress(count, block_size, total_size):
+        if count % 20 == 0:
+            print('Downloaded %02.02f/%02.02f MB' % (
+                count * block_size / 1024.0 / 1024.0,
+                total_size / 1024.0 / 1024.0), end='\r')
+
+    filepath, _ = urllib.request.urlretrieve(
+        path, filename=fname, reporthook=progress)
+    return filepath
+
+
+def download_and_extract_tar(path, dst):
+    """Download and extract a tar file.
+
+    Parameters
+    ----------
+    path : str
+        Url to tar file to download.
+    dst : str
+        Location to save tar file contents.
+    """
+    import tarfile
+    filepath = download(path)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        tarfile.open(filepath, 'r:gz').extractall(dst)
+
+
+def download_and_extract_zip(path, dst):
+    """Download and extract a zip file.
+
+    Parameters
+    ----------
+    path : str
+        Url to zip file to download.
+    dst : str
+        Location to save zip file contents.
+    """
+    import zipfile
+    filepath = download(path)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        zf = zipfile.ZipFile(file=filepath)
+        zf.extractall(dst)
+
+
 def load_audio(filename, b_normalize=True):
     """Load the audiofile at the provided filename using scipy.io.wavfile.
 
