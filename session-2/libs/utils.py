260901dc6a0cf2dc8d073c86dd376ef6f2ab594d
pkmital <parag@pkmital.com>
2017-03-27 17:03:57 -0400

    adding support for grayscale montages with singleton dimension

--- session-2/libs/utils.py
+++ session-2/libs/utils.py
@@ -12,6 +12,7 @@ import urllib
 import numpy as np
 import zipfile
 import os
+from scipy.misc import imsave
 
 
 def imcrop_tosquare(img):
@@ -93,10 +94,17 @@ def montage(images, saveto='montage.png'):
         m = np.ones(
             (images.shape[1] * n_plots + n_plots + 1,
              images.shape[2] * n_plots + n_plots + 1, 3)) * 0.5
-    else:
+    elif len(images.shape) == 4 and images.shape[3] == 1:
+        m = np.ones(
+            (images.shape[1] * n_plots + n_plots + 1,
+             images.shape[2] * n_plots + n_plots + 1, 1)) * 0.5
+    elif len(images.shape) == 3:
         m = np.ones(
             (images.shape[1] * n_plots + n_plots + 1,
              images.shape[2] * n_plots + n_plots + 1)) * 0.5
+    else:
+        raise ValueError('Could not parse image shape of {}'.format(
+            images.shape))
     for i in range(n_plots):
         for j in range(n_plots):
             this_filter = i * n_plots + j
@@ -104,7 +112,7 @@ def montage(images, saveto='montage.png'):
                 this_img = images[this_filter]
                 m[1 + i + i * img_h:1 + i + (i + 1) * img_h,
                   1 + j + j * img_w:1 + j + (j + 1) * img_w] = this_img
-    plt.imsave(arr=m, fname=saveto)
+    imsave(arr=np.squeeze(m), name=saveto)
     return m
 
 
