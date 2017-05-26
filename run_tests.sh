ad8fdab972e01e6cf2b5b6603d8d691193a82f3b
Parag K. Mital <parag@kadenze.com>
2017-05-26 09:48:06 -0700

    updates for new rnn stuff in 1.1+

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -it -v /$(pwd)/session-1:/notebooks cadl py.test --cache-clear 
-docker run -it -v /$(pwd)/session-2:/notebooks cadl py.test --cache-clear 
-docker run -it -v /$(pwd)/session-3:/notebooks cadl py.test --cache-clear 
-docker run -it -v /$(pwd)/session-4:/notebooks cadl py.test --cache-clear 
-docker run -it -v /$(pwd)/session-5:/notebooks cadl py.test --cache-clear 
+docker run --rm -it -v /$(pwd)/session-1:/notebooks cadl py.test --cache-clear 
+docker run --rm -it -v /$(pwd)/session-2:/notebooks cadl py.test --cache-clear 
+docker run --rm -it -v /$(pwd)/session-3:/notebooks cadl py.test --cache-clear 
+docker run --rm -it -v /$(pwd)/session-4:/notebooks cadl py.test --cache-clear 
+docker run --rm -it -v /$(pwd)/session-5:/notebooks cadl py.test --cache-clear 
